9d5375533b167bf9734397985b2d63aa0f17d7c5
Gabriel Vogado <gabriel.vogado_ntconsult@prestador.globo>
2023-06-14 22:24:41 -0300

    initial commit

---
+++ src/main/java/com/teste/dimensa/DimensaApplication.java
@@ -0,0 +1,13 @@
+package com.teste.dimensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimensaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimensaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/dimensa/DimensaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.dimensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
