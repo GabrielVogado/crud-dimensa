f76783299360820fdcd98753ab606672c1b1dad8
Gabriel Vogado <gabriel.vogado_ntconsult@prestador.globo>
2023-06-14 23:33:37 -0300

    Ajuste de Injeção de dependencia na Classe EnderecoController

--- src/main/java/com/teste/dimensa/controller/EnderecoController.java
+++ src/main/java/com/teste/dimensa/controller/EnderecoController.java
@@ -1,10 +1,16 @@
 package com.teste.dimensa.controller;
 
 import com.teste.dimensa.iservice.IEnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnderecoController {
 
     private IEnderecoService enderecoService;
+
+    @Autowired
+    public EnderecoController(IEnderecoService enderecoService) {
+        this.enderecoService = enderecoService;
+    }
 }
