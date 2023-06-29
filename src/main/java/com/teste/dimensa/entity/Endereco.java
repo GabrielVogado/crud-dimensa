524039e29b9e5ceaa191d51c765c1387c819ab78
Gabriel Vogado <gabriel.vogado_ntconsult@prestador.globo>
2023-06-28 22:03:22 -0300

    Validação de status code

--- src/main/java/com/teste/dimensa/entity/Endereco.java
+++ src/main/java/com/teste/dimensa/entity/Endereco.java
@@ -3,8 +3,6 @@ package com.teste.dimensa.entity;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
@@ -23,20 +21,14 @@ public class Endereco implements Serializable {
 
     @Column(name = "rua")
     @NotNull(message = "Rua não pode ser Nulo")
-    @NotBlank(message = "Rua não pode estar em Branco")
-    @NotEmpty(message = "Rua não pode estar Vazio")
     private String rua;
 
     @Column(name = "numero")
     @NotNull(message = "numero não pode ser Nulo")
-    @NotBlank(message = "numero não pode estar em Branco")
-    @NotEmpty(message = "numero não pode estar vazio")
     private String numero;
 
     @Column(name = "cep")
     @NotNull(message = "cep não pode ser Nulo")
-    @NotBlank(message = "cep não pode estar em Branco")
-    @NotEmpty(message = "cep não pode estar vazio")
     private String cep;
 
     @Column(name = "pessoa_id")
--- src/main/java/com/teste/dimensa/entity/Pessoa.java
+++ src/main/java/com/teste/dimensa/entity/Pessoa.java
@@ -4,8 +4,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
@@ -25,27 +23,20 @@ public class Pessoa implements Serializable {
 
     @Column(name = "nome")
     @NotNull(message = "nome não pode ser nulo")
-    @NotBlank(message = "nome não pode estar em Branco")
-    @NotEmpty(message = "nome não pode estar vazio")
     private String nome;
 
     @Column(name = "email")
     @NotNull(message = "email não pode ser nulo")
-    @NotBlank(message = "email não pode estar em Branco")
-    @NotEmpty(message = "email não pode estar  Vazio")
+
     @Email(message = "Invalid email")
     private String email;
 
     @Column(name = "telefone")
     @NotNull(message = "telefone não pode ser nulo")
-    @NotBlank(message = "telefone não pode estar em Branco")
-    @NotEmpty(message = "telefone não pode estar Vazio")
     private String telefone;
 
     @Column(name = "data_nascimento")
-    @NotBlank(message = "Data Nascimento não pode estar em Branco")
     @NotNull(message = "Data Nascimento não pode ser nulo")
-    @NotEmpty(message = "Data Nascimento não pode ser vazio")
     private String dataNascimento;
 
     @OneToMany(cascade = CascadeType.ALL)
